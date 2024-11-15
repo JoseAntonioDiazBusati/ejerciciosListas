e519b4ad6fd3a25efe8f2a9c062aad00bb861d37
JoseAntonio <jdiabus511@g.educaand.es>
2024-11-15 15:09:22 +0100

    Ejercicio 4

--- src/listas_tuplas/Ejercicio1_4.kt
+++ src/listas_tuplas/Ejercicio1_4.kt
@@ -5,8 +5,30 @@ package listas_tuplas
 * los almacene en una lista y los muestre por pantalla ordenados de menor a mayor.
 */
 
+fun ordenarGanadores(ganadores: MutableList<Int>){
+    val ganador = ganadores.size
+    for (i in 0 until ganador - 1) {
+        for (j in i until ganador) {
+            if (ganadores[i] > ganadores[j]) {
+                val temp = ganadores[j]
+                ganadores[j] = ganadores[i]
+                ganadores[i] = temp
+            }
+        }
+    }
+}
 
 
 fun main(args: Array<String>) {
-
+    val ganadores = mutableListOf<Int>()
+    println("Introduce los numeros ganadores:")
+    for (i in 0..5) {
+        val num = readln().toInt()
+        ganadores.add(num)
+    }
+    ordenarGanadores(ganadores)
+    println("\nAquí estan los numeros ganadores:")
+    for(i in ganadores.indices){
+        println("${ganadores[i]}")
+    }
 }
